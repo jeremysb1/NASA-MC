ad96367fbaa9aea113a0de5a361eb6f83c2d9080
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-06-18 17:44:11 -0400

    adding routing

---
+++ server/src/routes/launches/launches.controller.js
@@ -0,0 +1,9 @@
+const { launches } = require('../../models/launches.model');
+
+function httpGetAllLaunches(req, res) {
+  return res.status(200).json(Array.from(launches.values()));
+};
+
+module.exports = {
+  httpGetAllLaunches,
+};
