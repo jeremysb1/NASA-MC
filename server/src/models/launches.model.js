74965482b8a9a6cc4aa517b78e0bed6f96159ec3
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-06-18 19:22:47 -0400

    adding launch mvc data layer

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,12 +1,14 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
   destination: 'Kepler-442 b',
-  customer: ['NASA', 'Jimbo'],
+  customer: ['NASA', 'Wildman'],
   upcoming: true,
   success: true,
 };
@@ -15,6 +17,17 @@ launches.set(launch.flightNumber, launch);
 
 function getAllLaunches() {
   return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber, 
+    Object.assign(launch, {
+      customers: ['NASA', 'Wildman'],
+      flightNumber: latestFlightNumber,
+    })  
+  );
 }
 
 module.exports = {
