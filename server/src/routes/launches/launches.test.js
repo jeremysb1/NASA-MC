e47ad8dfa8b875d46ea8e3358f1d296a017ee8c6
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-06-21 13:00:34 -0400

    refactoring launch code and testing json

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -3,14 +3,15 @@ const app = require('../../app');
 
 describe('Test GET /Launches', () => {
   test('It should respond with 200 success', async () => {
-  	const response = await request(app).get('/launches')
-  	expect(response.statusCode).toBe(200);
+  	const response = await request(app)
+  	  .get('/launches')
+  	  .expect('Content-Type', /json/)
+  	  .expect(200);
   });
 });
 
 describe('Test POST /Launches', () => {
   test('Should respond with 200 success', () => {
-
   });
 
   test('Should catch missing required properties', () => {});
