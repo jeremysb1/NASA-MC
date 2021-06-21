8faffc91e90f275c09f8edd05ca82eeff685f969
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-06-21 12:26:57 -0400

    using describe function from jest docs to group a block of related tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,15 @@
 describe('Test GET /Launches', () => {
   test('It should respond with 200 success', () => {
   	const response = 200;
-  })
-})
+  	expect(response).toBe(200);
+  });
+});
 
-describe('Test POST /Launches')
+describe('Test POST /Launches', () => {
+  test('Should respond with 200 success', () => {
+
+  });
+
+  test('Should catch missing required properties', () => {});
+  test('Should catch invalid dates', () => {});
+});
