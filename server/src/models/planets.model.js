8bde11443f32edbe207068a0d0801d93a0e39adf
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-06-22 14:44:48 -0400

    connecting planets model to mongodb via mongoose

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const parse = require('csv-parse');
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo');
 
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
@@ -38,6 +38,20 @@ async function getAllPlanets() {
   return await planets.find({}, {
     '_id': 0, '__v': 0,
   });
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne({
+      keplerName: planet.kepler_name,
+    }, {
+      keplerName: planet.kepler_name,
+    }, {
+      upsert: true,
+    });
+  } catch(err) {
+    console.error(`Could not save planet ${err}`);
+  }
 }
 
 module.exports = {
