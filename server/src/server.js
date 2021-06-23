24e449a26ea1ee9ef0b931a5a00ea47724c15189
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-06-23 14:27:22 -0400

    cleaning up server code to improve performance and readability

--- server/src/server.js
+++ server/src/server.js
@@ -1,32 +1,18 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
 const app = require('./app');
-
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = 'mongodb+srv://nasa-api:vmeKrdzS57J7WVe@cluster0.q8zvm.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('connection ready...');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect();
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`listening on PORT: ${PORT}...`);
