7d49ce303b38631ac69b7b9fc26efeecb9caac97
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-06-23 10:32:24 -0400

    adding function to get launch data from db

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -21,8 +21,12 @@ function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getAllLaunches(skip, limit) {
+  return await launchesDatabase
+    .find({}, { '_id': 0, '__v': 0 })
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
